cc1ba1d0a535e5ea9a04187c1f325342057a77d3
widakso <widakso@me.com>
2017-03-05 00:36:24 +0700

    pllmull is 10
    change pllmull from 9 to 10.

--- system_stm32f37x.c
+++ system_stm32f37x.c
@@ -45,9 +45,9 @@
   *=============================================================================
   *        System Clock source                    | PLL (HSE)
   *-----------------------------------------------------------------------------
-  *        SYSCLK(Hz)                             | 72000000
+  *        SYSCLK(Hz)                             | 80000000
   *-----------------------------------------------------------------------------
-  *        HCLK(Hz)                               | 72000000
+  *        HCLK(Hz)                               | 80000000
   *-----------------------------------------------------------------------------
   *        AHB Prescaler                          | 1
   *-----------------------------------------------------------------------------
@@ -61,7 +61,7 @@
   *-----------------------------------------------------------------------------
   *        PREDIV                                 | 1
   *-----------------------------------------------------------------------------
-  *        I2S input clock(Hz)                    | 72000000
+  *        I2S input clock(Hz)                    | 80000000
   *                                               |
   *        To achieve the following I2S config:   |
   *         - Master clock output (MCKO): ON      |
@@ -145,7 +145,7 @@
 /** @addtogroup STM32F37x_System_Private_Variables
   * @{
   */
-uint32_t SystemCoreClock    = 72000000;
+uint32_t SystemCoreClock    = 80000000;
 __I uint8_t AHBPrescTable[16] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 6, 7, 8, 9};
 
 /**
@@ -346,7 +346,7 @@ static void SetSysClock(void)
 
     /* PLL configuration */
     RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_PLLSRC | RCC_CFGR_PLLXTPRE | RCC_CFGR_PLLMULL));
-    RCC->CFGR |= (uint32_t)(RCC_CFGR_PLLSRC_PREDIV1 | RCC_CFGR_PLLXTPRE_PREDIV1 | RCC_CFGR_PLLMULL9);
+    RCC->CFGR |= (uint32_t)(RCC_CFGR_PLLSRC_PREDIV1 | RCC_CFGR_PLLXTPRE_PREDIV1 | RCC_CFGR_PLLMULL10);
 
     /* Enable PLL */
     RCC->CR |= RCC_CR_PLLON;
